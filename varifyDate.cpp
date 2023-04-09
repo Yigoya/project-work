c64dd3aacfd5b293e0ce4f6eda2d635dcf000de4
Yihun Alemayehu <101422490+Yihun-Alemayehu@users.noreply.github.com>
2023-04-09 22:23:59 +0300

    Update varifyDate.cpp

--- varifyDate.cpp
+++ varifyDate.cpp
@@ -1,38 +1,84 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+bool leap(int y){
+    if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)){
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
-    for(int i=0;i<=9;i++){
-        int c=0;
-        while(c<=i){
-            cout<<" ";
-            c++;
-        }
-        for(int j=9-i;j>=0;j--){
-            cout<<j;
-        }
-        for(int j=1;j<=9-i;j++){
-            cout<<j;
-        }
-        cout<<endl;
+   int day,month,year;
+   bool isvalid=false;
+   string date;
+   cout<<"Please enter a date (dd mm yyyy eg. 01072023): \n";
+   cin>>date;
+   day = stoi(date.substr(0,2));
+   month = stoi(date.substr(2,2));
+   year = stoi(date.substr(4));
+   int* d = &day;
+   int* m = &month;
+   int* y = &year;
+
+
+    if((*d<=31 &&*d>=1) && (*m<=12&& *m>=1)){
+
+       switch(*d){
+        case 29: if(leap(*y)==false){cout<<"Invalid day of month 29"; break;}
+        case 28:
+
+            if(*m==2){
+                isvalid = true;
+            }
+
+            break;
+        case 30:
+            if(*m==4 || *m==6 || *m==9 || *m==11 ){
+                isvalid = true;
+            }
+            break;
+        case 31:
+            if(*m==1 || *m==3 || *m==5 || *m==7 || *m==8 || *m==10 || *m==12){
+                isvalid = true;
+            }
+            break;
+        default:
+            if(day<=27){
+            isvalid = true;
+            }
+            break;
+       }
     }
-    for(int i=9;i>=1;i--){
-        int c=1;
-        while(c<=i){
-            cout<<" ";
-            c++;
+    else{
+        if(day>31){
+            cout<<"Invalid day: "<<d<<endl;
         }
-        for(int j=9-i+1;j>=0;j--){
-            cout<<j;
+        if(*m>12){
+            cout<<"Invalid month: "<<m<<endl;
         }
-        for(int j=1;j<=9-i+1;j++){
-            cout<<j;
-        }
-        cout<<endl;
     }
 
 
+
+    if(isvalid && leap(*y)){
+        cout<<*d<<"/"<<*m<<"/"<<*y<<" is a valid date and also a Leap Year"<<endl;
+    }
+    else if(isvalid && leap(*y)==false){
+        cout<<*d<<"/"<<*m<<"/"<<*y<<" is a valid date but not a Leap Year"<<endl;
+    }
+    else{
+        cout<<"invalid date "<<*d<<"/"<<*m<<"/"<<*y<<endl;
+    }
+    if(leap(*y)){
+        int leap_year = *y;
+            cout<<"Here is below the next twenty Leap years are: "<<endl;
+        for(int i=0;i<20;i++){
+            leap_year+=4;
+            cout<<leap_year<<",";
+        }
+    }
     return 0;
 }
