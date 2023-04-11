22c4008bfa632429366b4e9c5f422a856fa127d4
Yigoya <123858017+Yigoya@users.noreply.github.com>
2023-04-11 11:28:52 +0300

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -4,30 +4,33 @@ using namespace std;
 
 int main()
 {
-    for(int i=0;i<=9;i++){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
         int c=0;
         while(c<=i){
             cout<<"  ";
             c++;
         }
-        for(int j=9-i;j>=0;j--){
+        for(int j=n-i;j>=0;j--){
             cout<<j<<' ';
         }
-        for(int j=1;j<=9-i;j++){
+        for(int j=1;j<=n-i;j++){
             cout<<j<<' ';
         }
         cout<<endl;
     }
-    for(int i=9;i>=1;i--){
+    for(int i=n;i>=1;i--){
         int c=1;
         while(c<=i){
             cout<<"  ";
             c++;
         }
-        for(int j=9-i+1;j>=0;j--){
+        for(int j=n-i+1;j>=0;j--){
             cout<<j<<' ';
         }
-        for(int j=1;j<=9-i+1;j++){
+        for(int j=1;j<=n-i+1;j++){
             cout<<j<<' ';
         }
         cout<<endl;
